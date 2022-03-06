52f71544d9bfd55186f6bbac0054522bfd96b439
Darian Harrison <darianharrison89@gmail.com>
2022-03-06 10:33:53 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -74,25 +74,33 @@ fn main() {
     println!("{:08b}", &op_1);
 
     /////////////////////
-    // log2
-    //Returns the base 2 logarithm of the number, rounded down.
-#![feature(int_log)]
-assert_eq!(2u8.log2(), 1);
+    // // log2
+    // //Returns the base 2 logarithm of the number, rounded down.
+    // #![feature(int_log)]
+    // assert_eq!(2u8.log2(), 1);
 
-    // log10
-    // Returns the base 10 logarithm of the number, rounded down.
-#![feature(int_log)]
-assert_eq!(10u8.log10(), 1);
+    // // log10
+    // // Returns the base 10 logarithm of the number, rounded down.
+    // #![feature(int_log)]
+    // assert_eq!(10u8.log10(), 1);
 
 
-    // Saturating integer addition.
-    // Computes self + rhs, saturating at the numeric bounds instead of overflowing.
-assert_eq!(100u8.saturating_add(1), 101);
-assert_eq!(u8::MAX.saturating_add(127), u8::MAX);
+    // // Saturating integer addition.
+    // // Computes self + rhs, saturating at the numeric bounds instead of overflowing.
+    // assert_eq!(100u8.saturating_add(1), 101);
+    // assert_eq!(u8::MAX.saturating_add(127), u8::MAX);
 
-    // Saturating integer subtraction. 
-    // Computes self - rhs, saturating at the numeric bounds instead of overflowing.
-assert_eq!(100u8.saturating_sub(27), 73);
-assert_eq!(13u8.saturating_sub(127), 0);
+    // // Saturating integer subtraction. 
+    // // Computes self - rhs, saturating at the numeric bounds instead of overflowing.
+    // assert_eq!(100u8.saturating_sub(27), 73);
+    // assert_eq!(13u8.saturating_sub(127), 0);
+
+Saturating integer multiplication. Computes self * rhs, saturating at the numeric bounds instead of overflowing.
+assert_eq!(2u8.saturating_mul(10), 20);
+assert_eq!((u8::MAX).saturating_mul(10), u8::MAX);
+
+
+Saturating integer division. Computes self / rhs, saturating at the numeric bounds instead of overflowing.
+assert_eq!(5u8.saturating_div(2), 2);
 
 }
